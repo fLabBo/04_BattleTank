eb559429afe4dc0ed8204ae61e819b465adf7b84
Rahni Tucker <r4hnitucker@gmail.com>
2017-11-30 07:43:52 +0000

    added unreal game files

---
+++ BTGame/Source/BTGame/BTGameGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "BTGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BTGAME_API ABTGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
